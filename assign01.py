c0d851c22c77e107f5bd12e201efb1fba8b95b30
Jacob Cormier <cormiej@sunypoly.edu>
2026-02-17 15:22:40 -0500

    assign01 setup

---
+++ assign01.py
@@ -0,0 +1,23 @@
+def get_log_transform(max_r):
+    pass
+
+def get_gamma_transform(gamma):
+    pass
+
+def get_hist_equalize_transform(image, do_stretching):
+    pass
+
+def get_piecewise_linear_transform(points):
+    pass
+
+def apply_intensity_transform(image, int_transform):
+    pass
+
+def estimate_gamma_exponent(image, output):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
