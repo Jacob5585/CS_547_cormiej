7a3bf9439874c3e4eb1dbdb20501c7efa28b7dac
jacob5585 <jacobcormier02@gmail.com>
2026-03-14 13:19:01 -0400

    A02 setup

---
+++ A02.py
@@ -0,0 +1,26 @@
+def read_kernel(filepath):
+    pass
+
+def do_convolution_slow(image, kernel, alpha=1.0, beta=0.0, convert_unit8=True):
+    pass
+
+def do_convolution_fast(image, kernel, alpha=1.0, beta=0.0, convert_unit8=True):
+    pass
+
+def do_convolution_dourier(image, kernel, alpha=1.0, beta=0.0, convert_unit8=True):
+    pass
+
+def check_if_seperable(kernel):
+    pass
+
+def do_convolution_separable(image, kernel, alpha=1.0, beta=0.0, convert_unit8=True, conv_func=do_convolution_fast):
+    pass
+
+def do_convolution_optimal(image, kernel, alpha=1.0, beta=0.0, convert_unit8=True):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
