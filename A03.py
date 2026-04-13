8bb598f8b0b2d91726bd0946d890bcd564c85e94
Jacob Cormier <cormiej@sunypoly.edu>
2026-04-13 11:26:04 -0400

    setup

---
+++ A03.py
@@ -0,0 +1,16 @@
+class CellFinder():
+    def __init__(self, model_dir):
+        pass
+    
+    def train_WBC(self, train_data):
+        pass
+
+    def find_WBC(self, image):
+        pass
+
+    def train_RBC(self, train_data):
+        pass
+
+    def find_RBC(self, image):
+        pass
+
