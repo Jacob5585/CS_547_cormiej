c7b326b5782f74770a9fd5900e31674f78c71144
jacob5585 <jcormier02@gmail.com>
2026-04-29 09:06:51 -0400

    Initial Model

--- A04.py
+++ A04.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn as nn
 import torchvision
-from torchvision.transform import v2
+from torchvision import transforms
+from torchvision.transforms import v2
 
 def get_approach_names():
     approches = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8"]
@@ -29,35 +31,68 @@ def get_approach_description(approach_name):
             return "Not a valud approch"    
 
 def get_data_transform(approach_name, training):
+    # pytroch 130
+    # data_transform = v2.Compose([v2.ToImageTensor(), v2.ConvertImageDtype()])
+    # data_transform = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+        
+    # pytorch 121
+    data_transform = transforms.Compose([transforms.ToTensor(),])
+    
     if training:
-        data_transform = v2.Compose([v2.ToImageTensor(), v2.ConvertImageDtype()])
-        data_transform = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+        pass   
+
+    return data_transform
 
 def get_batch_size(approach_name):
     match approach_name:
         case "a1":
             return 10
         case "a2":
-            return 20
+            return 10
         case "a3":
-            return 30
+            return 10
         case "a4":
-            return 40
+            return 10
         case "a5":
-            return 25
+            return 15
         case "a6":
-            return 5
+            return 10
         case "a7":
-            return 1
+            return 10
         case "a8":
-            return 50
+            return 10
         case _:
             return "Not a valud approch" 
 
 def create_model(approach_name, class_cnt):
     match approach_name:
         case "a1":
-            pass
+            return nn.Sequential(
+                # layer 1
+                nn.Conv2d(3, 32, kernel_size=3, padding=1), # takes 3 channel 32x32 image
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2), # 16x16
+
+                # layer 2
+                nn.Conv2d(32, 64, kernel_size=3, padding=1), # takes 3 channel 32x32 image
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2), # 8x8
+
+                # layer 3
+                nn.Conv2d(64, 128, kernel_size=3, padding=1), # takes 3 channel 32x32 image
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2), # 4x4
+
+                nn.Flatten(),
+
+                # layer 4 Fully Connected 
+                nn.Linear(128 * 4 * 4, 256),
+                nn.ReLU(),
+
+                # Output
+                nn.Linear(256, class_cnt)
+            )
+
         case "a2":
             pass
         case "a3":
@@ -76,4 +111,19 @@ def create_model(approach_name, class_cnt):
             return "Not a valud approch" 
 
 def train_model(approach_name, model, device, train_dataloader, test_dataloader):
-    pass
+    criterion = nn.CrossEntropyLoss()
+    optimzer = torch.optim.Adam(model.parameters(), lr=0.001)
+    epochs = 10
+    model.to(device)
+
+    for epoch in range(epochs):
+        model.train()
+        for images, labels in train_dataloader:
+            images, labels = images.to(device), labels.to(device)
+            optimzer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimzer.step()
+    
+    return model        
